8cf76a3832a7fc9331069efa1a7568b3ca4c6f52
Shinichi Masuda <masuda@my-style.jp>
2022-10-17 14:52:51 +0000

    ログイン実装中

--- src/controllers/auth/loginController.ts
+++ src/controllers/auth/loginController.ts
@@ -3,17 +3,13 @@ import loginService from '../../services/auth/loginService';
 
 export const login = async (req: Request, res: Response): Promise<Response> => {
   try {
-    console.log(req.body);
-    // res.header('Content-Type', 'application/json; charset=utf-8');
-
     const email = req.body.email;
     const password = req.body.password;
     const service = new loginService();
     const user = await service.login(email, password);
-    // let token;
     if (user) {
       const token = service.makeToken({ email: user.email });
-      return res.send({ data: token });
+      return res.send({ token: token });
     } else {
       return res.status(400).end();
     }
